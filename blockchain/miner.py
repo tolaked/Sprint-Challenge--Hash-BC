ddb7186c80baf3d40488a4d9446fcc8d555ff21d
Akere Adetola <adetolaakere1@gmail.com>
2020-03-13 12:46:54 +0100

    refactored miner.py

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,9 +24,9 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    while valid_proof(last_proof, proof):
+    last_hash = hashlib.sha256(f"{last_proof}".encode()).hexdigest()
+    while valid_proof(last_hash, proof) is False:
         proof += 1
-
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
 
@@ -40,12 +40,10 @@ def valid_proof(last_hash, proof):
     IE:  last_hash: ...AE9123456, new hash 123456E88...
     """
 
-    guess = f"{last_hash}{proof}".encode()
-    # hashing the guess
+    guess = f"{proof}".encode()
     guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == last_hash[-6:]
 
-    print('Old guess', last_hash)
-    print('New guess', guess_hash[0:6])
 
 
 if __name__ == '__main__':
